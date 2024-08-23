cb58d3f55ac69f490161f3a47c6d2bffa65466ad
onkarabile masoko <onkarabilemasoko1@gmail.com>
2024-08-23 14:35:23 +0200

    Add routing structure for the application with key pages and components
     Implement routing using `react-router-dom` in the `App` component.
     Add routes for `LoginPage`, `HomePage`, `PodcastPage`, `GenrePage`, `FavouritesPage`, and `SearchResults`.
     Include a `Header` component that displays on all pages except the login page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import PodcastPage from './components/PodcastPage';
+import GenrePage from './components/GenrePage';
+import FavouritesPage from './pages/FavouritesPage';
+import Header from './components/Header';
+import LoginPage from './pages/LoginPage';
+import SearchResults from './pages/SearchResults';
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <Header />
+      <Routes>
+        <Route path="/" element={<LoginPage />} /> {/* Login page */}
+        <Route path="/home" element={<HomePage />} /> {/* Homepage */}
+        <Route path="/podcast/:id" element={<PodcastPage />} />
+        <Route path="/genre/:id" element={<GenrePage />} />
+        <Route path="/favourites" element={<FavouritesPage />} /> {/* Favourites page */}
+        <Route path="/search" element={<SearchResults />} />
 
-export default App
+      </Routes>
+    </>
+  );
+};
+
+export default App;
