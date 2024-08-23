7162dee87102e78dfe8af7d15e377cfcd873efcd
onkarabile masoko <onkarabilemasoko1@gmail.com>
2024-08-23 14:53:50 +0200

    I added styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        navy: {
+          750: '#1a2638',
+          800: '#1e2a3a',
+          900: '#0f172a',
+        },
+      },
+    },
   },
   plugins: [],
 }
-
